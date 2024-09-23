9dbd08e8491a381bddd7838758f851fcbe487876
max <max@swk-web.com>
2024-09-23 11:10:10 +0200

    refactor: Cache constants

--- src/Core/Framework/Adapter/Cache/Http/CacheResponseSubscriber.php
+++ src/Core/Framework/Adapter/Cache/Http/CacheResponseSubscriber.php
@@ -33,10 +33,19 @@ class CacheResponseSubscriber implements EventSubscriberInterface
     final public const STATE_LOGGED_IN = CacheStateSubscriber::STATE_LOGGED_IN;
     final public const STATE_CART_FILLED = CacheStateSubscriber::STATE_CART_FILLED;
 
-    final public const CURRENCY_COOKIE = 'sw-currency';
+    /**
+     * @deprecated  Use HttpCacheKeyGenerator::CURRENCY_COOKIE
+     */
+    final public const CURRENCY_COOKIE = HttpCacheKeyGenerator::CURRENCY_COOKIE;
+    /**
+     * @deprecated  Use HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE
+     */
     final public const CONTEXT_CACHE_COOKIE = HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE;
 
-    final public const SYSTEM_STATE_COOKIE = 'sw-states';
+    /**
+     * @deprecated  Use HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE
+     */
+    final public const SYSTEM_STATE_COOKIE = HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE;
     /**
      * @deprecated tag:v6.7.0 - Will be removed
      */
@@ -121,15 +130,15 @@ class CacheResponseSubscriber implements EventSubscriberInterface
         if ($context->getCustomer() || $cart->getLineItems()->count() > 0) {
             $newValue = $this->buildCacheHash($request, $context);
 
-            if ($request->cookies->get(self::CONTEXT_CACHE_COOKIE, '') !== $newValue) {
-                $cookie = Cookie::create(self::CONTEXT_CACHE_COOKIE, $newValue);
+            if ($request->cookies->get(HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE, '') !== $newValue) {
+                $cookie = Cookie::create(HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE, $newValue);
                 $cookie->setSecureDefault($request->isSecure());
 
                 $response->headers->setCookie($cookie);
             }
-        } elseif ($request->cookies->has(self::CONTEXT_CACHE_COOKIE)) {
-            $response->headers->removeCookie(self::CONTEXT_CACHE_COOKIE);
-            $response->headers->clearCookie(self::CONTEXT_CACHE_COOKIE);
+        } elseif ($request->cookies->has(HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE)) {
+            $response->headers->removeCookie(HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE);
+            $response->headers->clearCookie(HttpCacheKeyGenerator::CONTEXT_CACHE_COOKIE);
         }
 
         /** @var bool|array{maxAge?: int, states?: list<string>}|null $cache */
@@ -150,7 +159,7 @@ class CacheResponseSubscriber implements EventSubscriberInterface
 
         $response->setSharedMaxAge($maxAge);
         $response->headers->set(
-            self::INVALIDATION_STATES_HEADER,
+            HttpCacheKeyGenerator::INVALIDATION_STATES_HEADER,
             implode(',', $cache['states'] ?? [])
         );
 
@@ -265,9 +274,9 @@ class CacheResponseSubscriber implements EventSubscriberInterface
         $states = $this->getSystemStates($request, $context, $cart);
 
         if (empty($states)) {
-            if ($request->cookies->has(self::SYSTEM_STATE_COOKIE)) {
-                $response->headers->removeCookie(self::SYSTEM_STATE_COOKIE);
-                $response->headers->clearCookie(self::SYSTEM_STATE_COOKIE);
+            if ($request->cookies->has(HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE)) {
+                $response->headers->removeCookie(HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE);
+                $response->headers->clearCookie(HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE);
             }
 
             return [];
@@ -275,8 +284,8 @@ class CacheResponseSubscriber implements EventSubscriberInterface
 
         $newStates = implode(',', $states);
 
-        if ($request->cookies->get(self::SYSTEM_STATE_COOKIE) !== $newStates) {
-            $cookie = Cookie::create(self::SYSTEM_STATE_COOKIE, $newStates);
+        if ($request->cookies->get(HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE) !== $newStates) {
+            $cookie = Cookie::create(HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE, $newStates);
             $cookie->setSecureDefault($request->isSecure());
 
             $response->headers->setCookie($cookie);
@@ -291,7 +300,7 @@ class CacheResponseSubscriber implements EventSubscriberInterface
     private function getSystemStates(Request $request, SalesChannelContext $context, Cart $cart): array
     {
         $states = [];
-        $swStates = (string) $request->cookies->get(self::SYSTEM_STATE_COOKIE);
+        $swStates = (string) $request->cookies->get(HttpCacheKeyGenerator::SYSTEM_STATE_COOKIE);
         if ($swStates !== '') {
             $states = array_flip(explode(',', $swStates));
         }
@@ -329,7 +338,7 @@ class CacheResponseSubscriber implements EventSubscriberInterface
             return;
         }
 
-        $cookie = Cookie::create(self::CURRENCY_COOKIE, $currencyId);
+        $cookie = Cookie::create(HttpCacheKeyGenerator::CURRENCY_COOKIE, $currencyId);
         $cookie->setSecureDefault($request->isSecure());
 
         $response->headers->setCookie($cookie);
